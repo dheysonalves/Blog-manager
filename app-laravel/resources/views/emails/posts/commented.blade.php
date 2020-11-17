f934c392c196271d0ed3f44e0b6bf9e947f2baa7
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-11-17 07:02:16 -0300

    feat: Create mail file

---
+++ app-laravel/resources/views/emails/posts/commented.blade.php
@@ -0,0 +1,28 @@
+<style>
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
+
+<p>
+    Hi {{ $comment->comentable->user->name }}
+</p>
+
+<p>
+    Someone has commented on your blog post!
+    <a href="{{ route('post.show', ['post' => $comment->commentable->id])}}">
+        {{ $comment->commentable->title }}
+    </a>
+</p>
+
+<hr />
+
+<p>
+    <a href="{{ route('users.show', ['user' => $comment->user->id ]) }}">
+        {{ $comment->user->name }}
+    </a> said:
+</p>
+
+<p>
+    "{{ $comment->content}}"
+</p>
