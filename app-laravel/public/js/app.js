f8dd77ad9ece876ea175a8ae504c135902581e48
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-02-03 22:43:15 -0300

    feat: adding browsersync in the laravel application

--- app-laravel/public/js/app.js
+++ app-laravel/public/js/app.js
@@ -37341,8 +37341,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/ubuntu-wsl2004dev/Development/Personal/courses/php-laravel-course/app-laravel/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/ubuntu-wsl2004dev/Development/Personal/courses/php-laravel-course/app-laravel/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\Blog-manager\app-laravel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\Blog-manager\app-laravel\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- app-laravel/resources/views/contact.blade.php
+++ app-laravel/resources/views/contact.blade.php
@@ -4,10 +4,10 @@
 <div class="container py-5">
     <img src="https://source.unsplash.com/random/1260x720" class="img-fluid" alt="Responsive image">
     <h1 class="display-1">
-       We are in contact
+       We are in contact aaaaaaaaaaczczdasdsadfffaasa
     </h1>
     <p class="lead">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolore beatae id aspernatur. Earum facere harum, adipisci omnis sint accusamus? Maiores quae ab labore laborum nam. Sed suscipit sunt enim.
+        Lorem, ipsum dolor sit amet aaaconsectetur adipisicing elit. Cupiditate dolore beatae id aspernatur. Earum facere harum, adipisci omnis sint accusamus? Maiores quae ab labore laborum nam. Sed suscipit sunt enim. adasdd
     </p>
 </div>
 @endsection
